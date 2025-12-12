5f9145e1db9a9008ef52ca8e5a369c99a1525ffb
kirillfomin0166-tech <kirillfomin0166@gmail.com>
2025-12-12 20:28:14 +0300

    Remove local run block from app.py
    Removed local run configuration for Flask app.

--- app.py
+++ app.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from Flask on Render!"
-    
-if __name__ == "__main__":
-    # Локальный запуск
-    app.run(host="0.0.0.0", port=10000)
