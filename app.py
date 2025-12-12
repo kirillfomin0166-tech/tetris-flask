755512a422f69bb0ae28b0e1d08f590c21573f5d
kirillfomin0166-tech <kirillfomin0166@gmail.com>
2025-12-12 20:22:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from Flask on Render!"
-
+    
 if __name__ == "__main__":
+    # Локальный запуск
     app.run(host="0.0.0.0", port=10000)
